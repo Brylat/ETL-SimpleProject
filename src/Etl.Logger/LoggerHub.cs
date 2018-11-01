30a1206f6891600ca914231bc60bcfecbcd28d27
Brylat <brylat8@hotmail.com>
2018-11-01 12:00:07 +0100

    delete uncecessary method

--- src/Etl.Logger/LoggerHub.cs
+++ src/Etl.Logger/LoggerHub.cs
@@ -3,8 +3,5 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Etl.Logger {
     public class LoggerHub : Hub {
-        public void Log (string message) {
-            Clients.All.SendAsync ("sendToAll", message);
-        }
     }
 }
